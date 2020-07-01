5b529105a06055ecb8098aa4e404da29213ef4b2
stepperL <xiaoyli@outlook.com>
2020-07-01 14:40:02 +0800

    fix/typos

--- scripts/tpu_mention_proposal.sh
+++ scripts/tpu_mention_proposal.sh
@@ -10,7 +10,7 @@ export PYTHONPATH="$PYTHONPATH:/home/xiaoyli1110/xiaoya/Coref-tf"
 # export TPU_NAME=tensorflow-tpu
 export TPU_NAME=tf-tpu
 GCP_PROJECT=xiaoyli-20-04-274510
-OUTPUT_DIR=gs://mention_proposal/output_30
+OUTPUT_DIR=gs://mention_proposal/output_1
 
 
 
